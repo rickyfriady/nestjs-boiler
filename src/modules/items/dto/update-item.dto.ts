f513d282f64a53eed609ad56689649f6241e4082
ricki friadi <friadi.ricki@gmail.com>
2024-05-24 16:06:47 +0700

    Fix : Setup package and swagger

--- src/modules/items/dto/update-item.dto.ts
+++ src/modules/items/dto/update-item.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
 import { CreateItemDto } from './create-item.dto';
+import { OmitType, PartialType } from '@nestjs/swagger';
 
-export class UpdateItemDto extends PartialType(CreateItemDto) {}
+export class UpdateItemDto extends PartialType(
+  OmitType(CreateItemDto, ['id'] as const),
+) {}
